c302136b134d2b1733cfae87b2dd648185863f43
Danilo Ariafina <ariafinadanilo4bma@gmail.com>
2019-01-24 16:14:08 +0000

    funzione delete funzionante

--- webapp/controller/Page2.controller.js
+++ webapp/controller/Page2.controller.js
@@ -4,11 +4,11 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History"
 ], function(BaseController, MessageBox, Utilities, History) {
 	"use strict";
-
+   
 	return BaseController.extend("com.sap.build.standard.untitledPrototype.controller.Page2", {
 		handleRouteMatched: function(oEvent) {
 			var sAppId = "App5c404e36a812130111d6a15e";
-
+     
 			var oParams = {};
 
 			if (oEvent.mParameters.data.context) {
@@ -51,18 +51,46 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 		
 		oEntry.CustId = oBindingContext.getProperty("CustId");
 		oEntry.TelNum = oBindingContext.getProperty("TelNum");
+  //      oEntry.CustName = oBindingContext.getProperty("CustName");
+		// oEntry.CustSur = oBindingContext.getProperty("CustSur");
+		// oEntry.CustAdd = oBindingContext.getProperty("CustAdd");
+		// oEntry.CustCity = oBindingContext.getProperty("CustCity");
+		// oEntry.CustProv = oBindingContext.getProperty("CustProv");
+		// oEntry.CustCAP = oBindingContext.getProperty("CustCAP");
+		// oEntry.CustStat = oBindingContext.getProperty("CustStat");
 		
 		
-		oModel.remove('/AnagraficaSet',oEntry,null,null,null);
+		// oModel.remove('/AnagraficaSet',oEntry,null,this.onCreateOk,this.onCreateKo);
 		
-		
+		var sPath = oBindingContext.getPath();
+		oModel.remove(sPath,{success:this.onCreateOk(),error:this.onCreateKo});
 		
 			
 		},
+		  onCreateOk: function(oEvent) {
+   	var msg = 'Eliminato';
+   	var that = this;
+   	// MessageBox.show(msg,{ onClose:this.navBack() });
+   MessageBox.show(msg,{ onClose: function(oAction){
+   	if (oAction === 'OK'){
+   	    var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
+    oRouter.navTo("Page1");}
+   } });
+   },
+   onCreateKo: function(oEvent) {
+   var msg = 'Errore';
+   	sap.m.MessageBox.show(msg);	
+   	
+   },
+   navBack: function(oAction){
+   	
+   	    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    oRouter.navTo("Page1");
+   },
 		onInit: function() {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("Page2").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
-
+            
 		}
 	});
 }, /* bExport= */ true);
